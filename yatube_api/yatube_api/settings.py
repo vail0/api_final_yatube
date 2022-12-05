598481bb2d31d383946f8e53107172a17459b6cc
vail0 <97312541+vail0@users.noreply.github.com>
2022-12-05 12:09:14 +0300

    models and admin in posts folder

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -15,7 +15,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'djoser',
     'api',
     'posts',
 ]
